10ff35b19474d617c4d3fc7860888cd4e147ca1c
purpura112 <amocarnecrua@gmail.com>
2025-06-04 21:38:33 -0300

    name change to easily see

--- Section6/exercicio_56/app.py
+++ Section6/exercicio_56/app.py
@@ -2,8 +2,8 @@ def data(name, age, job, func):
     greeting = func(name, age, job)
     return greeting
 
-def show_data(name, age, job):
-    greet = f"Hello, my name is {name}, I am {age} years old and I work as a {job}."
+def show_data(a, b, c):
+    greet = f"Hello, my name is {a}, I am {b} years old and I work as a {c}."
     return greet
 
 print(data("Alice", 30, "Engineer", show_data))
